a6e80a366ae43abc247242ddfc5725affcfa9c97
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 19:04:18 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,31 +3,31 @@
                     const bodyParser = require('body-parser');
                     const path = require('path');
 
-                    // Database
+                    
                   const db = require('./config/database');
 
-              // Test DB
+              
               db.authenticate()
       const app = express();
 
-      // Handlebars
+     
       app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
       app.set('view engine', 'handlebause(bodyParser.urlencoded({ extended: false }));
 
-            // Set static folder
+            
             app.use(express.static(path.join(__dirname, 'public')));
 
-            // Index routers');
+            
         app.use('/gigs', require('./routes/gigs'));
 
         const PORT = process.env.PORT || 5000;
 
         app.listen(PORT, console.log(`Server started on port ${PORT}`));
-      // Body Parser
+      
         .then(() => console.log('Database connected...'))
         .catch(err => console.log('Error: ' + err))
 
             app.
             app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
 
-            // Gig routes
+           
