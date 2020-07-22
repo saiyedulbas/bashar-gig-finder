61e7be4ace0d47fb09e0478e30549ca1f2c34617
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 19:07:35 +0600

    Update gigs.js

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,81 +1,81 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../config/database');
-const Gig = require('../models/Gig');
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
+              const express = require('express');
+                    const router = express.Router();
+                      const db = require('../config/database');
+                      const Gig = require('../models/Gig');
+                      const Sequelize = require('sequelize');
+              const Op = Sequelize.Op;
 
-// Get gig list
-router.get('/', (req, res) => 
-  Gig.findAll()
-    .then(gigs => res.render('gigs', { gigs: gigs }))
-    .catch(err => res.render('error', {error: err})));
 
-// Display add gig form
-router.get('/add', (req, res) => res.render('add'));
+      router.get('/', (req, res) => 
+        Gig.findAll()
+          .then(gigs => res.render('gigs', { gigs: gigs }))
+          .catch(err => res.render('error', {error: err})));
 
-// Add a gig
-router.post('/add', (req, res) => {
-  let { title, technologies, budget, description, contact_email } = req.body;
-  let errors = [];
 
-  // Validate Fields
-  if(!title) {
-    errors.push({ text: 'Please add a title' });
-  }
-  if(!technologies) {
-    errors.push({ text: 'Please add some technologies' });
-  }
-  if(!description) {
-    errors.push({ text: 'Please add a description' });
-  }
-  if(!contact_email) {
-    errors.push({ text: 'Please add a contact email' });
-  }
+      router.get('/add', (req, res) => res.render('add'));
 
-  // Check for errors
-  if(errors.length > 0) {
-    res.render('add', {
-      errors,
-      title, 
-      technologies, 
-      budget, 
-      description, 
-      contact_email
-    });
-  } else {
-    if(!budget) {
-      budget = 'Unknown';
-    } else {
-      budget = `$${budget}`;
-    }
 
-    // Make lowercase and remove space after comma
-    technologies = technologies.toLowerCase().replace(/,[ ]+/g, ',');
+                router.post('/add', (req, res) => {
+                  let { title, technologies, budget, description, contact_email } = req.body;
+                  let errors = [];
 
-    // Insert into table
-    Gig.create({
-      title,
-      technologies,
-      description,
-      budget,
-      contact_email
-    })
-      .then(gig => res.redirect('/gigs'))
-      .catch(err => res.render('error', {error:err.message}))
-  }
-});
 
-// Search for gigs
-router.get('/search', (req, res) => {
-  let { term } = req.query;
+                        if(!title) {
+                          errors.push({ text: 'Please add a title' });
+                        }
+                        if(!technologies) {
+                          errors.push({ text: 'Please add some technologies' });
+                        }
+        if(!description) {
+          errors.push({ text: 'Please add a description' });
+        }
+        if(!contact_email) {
+          errors.push({ text: 'Please add a contact email' });
+        }
 
-  // Make lowercase
-  term = term.toLowerCase();
+  
+                  if(errors.length > 0) {
+                    res.render('add', {
+                      errors,
+                      title, 
+                      technologies, 
+                      budget, 
+                      description, 
+                      contact_email
+                    });
+                  } else {
+                    if(!budget) {
+                            budget = 'Unknown';
+                          } else {
+                            budget = `$${budget}`;
+                          }
 
-  Gig.findAll({ where: { technologies: { [Op.like]: '%' + term + '%' } } })
-    .then(gigs => res.render('gigs', { gigs }))
-    .catch(err => res.render('error', {error: err}));
-});
 
-module.exports = router;
+                          technologies = technologies.toLowerCase().replace(/,[ ]+/g, ',');
+
+
+                          Gig.create({
+                            title,
+                            technologies,
+                            description,
+                            budget,
+                            contact_email
+                          })
+                            .then(gig => res.redirect('/gigs'))
+                            .catch(err => res.render('error', {error:err.message}))
+                        }
+                      });
+
+
+                      router.get('/search', (req, res) => {
+                        let { term } = req.query;
+
+
+                        term = term.toLowerCase();
+
+                  Gig.findAll({ where: { technologies: { [Op.like]: '%' + term + '%' } } })
+                    .then(gigs => res.render('gigs', { gigs }))
+                    .catch(err => res.render('error', {error: err}));
+      });
+
+      module.exports = router;
